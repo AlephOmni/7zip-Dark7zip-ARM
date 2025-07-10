f714870f1a32049324f5a6fb91c50a1e99e58bc8
AlephOmni <alephomni@proton.me>
2025-07-10 19:28:07 +0200

    Updated build file.

--- build.cmd
+++ build.cmd
@@ -65,9 +65,36 @@ if not "%2" == "fluent" (
   copy "LICENSE.md" "%PLATFORM%-bin"
 )
 
-rem Build the fluent version only for x64
+rem Build the fluent version for x64 and arm64
 if "%PLATFORM%" == "x64" (
   echo Building fluent version
+  if not exist "tmp\" mkdir "tmp"
+  move "CPP\7zip\UI\FileManager\*.bmp" "tmp" >nul
+  xcopy "DarkMode\7zRes\icons\*.bmp" "CPP\7zip\UI\FileManager" /Y >nul
+
+  if not "%2" == "fluent" (
+    if exist "CPP\7zip\UI\FileManager\%PLATFORM%\resource.res" (
+      del /F /Q "CPP\7zip\UI\FileManager\%PLATFORM%\resource.res"
+    )
+  )
+  
+  pushd CPP\7zip\UI\FileManager
+  nmake
+  popd
+
+  if not exist "%PLATFORM%-fluent-bin\" mkdir "%PLATFORM%-fluent-bin"
+  copy "CPP\7zip\Bundles\SFXWin\%PLATFORM%\7z.sfx" "%PLATFORM%-fluent-bin"
+  copy "CPP\7zip\UI\FileManager\%PLATFORM%\7zFM.exe" "%PLATFORM%-fluent-bin"
+  copy "CPP\7zip\UI\GUI\%PLATFORM%\7zG.exe" "%PLATFORM%-fluent-bin"
+  copy "DarkMode\7zRes\7zDark.ini" "%PLATFORM%-fluent-bin"
+  copy "LICENSE.md" "%PLATFORM%-fluent-bin"
+
+  xcopy "tmp\*.bmp" "CPP\7zip\UI\FileManager" /Y >nul
+  rmdir /S /Q "tmp"
+)
+
+if "%PLATFORM%" == "arm64" (
+  echo Building fluent version for ARM64
   if not exist "tmp\" mkdir "tmp"
   move "CPP\7zip\UI\FileManager\*.bmp" "tmp" >nul
   xcopy "DarkMode\7zRes\icons\*.bmp" "CPP\7zip\UI\FileManager" /Y >nul
